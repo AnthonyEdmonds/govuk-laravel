e892e3aa08853f8a724611792b70317eab13def3
Anthony Edmonds <anthony.edmonds@networkrail.co.uk>
2021-11-23 08:47:48 +0000

    Began revision of table component

--- src/resources/views/components/table.blade.php
+++ src/resources/views/components/table.blade.php
@@ -1,29 +1,3 @@
-@props([
-    'caption',
-    'captionSize' => 'm',
-    'data' => [],
-    'emptyMessage' => null,
-])
-
-@php
-    $html = $slot;
-    $columns = [];
-
-    do {
-        $start = strpos($html, '~~');
-
-        if ($start === false) {
-            break;
-        }
-
-        $end = strpos($html, '~~', $start + 1);
-        $length = $end - $start;
-
-        $columns[] = json_decode(substr($html, $start + 2, $length - 2));
-        $html = substr_replace($html, '', $start, $length + 2);
-    } while ($start !== false);
-@endphp
-
 <table class="govuk-table">
     <caption
         class="govuk-table__caption govuk-table__caption--{{ $captionSize }}"
@@ -31,13 +5,5 @@
         {{ $caption }}
     </caption>
     
-    <x-govuk::table.header
-        :columns="$columns"
-    />
-    
-    <x-govuk::table.body
-        :columns="$columns"
-        :rows="$data"
-        :empty-message="$emptyMessage"
-    />
+    @dd($data, $rows, $pagination)
 </table>
