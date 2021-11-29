994b1705c416908746fbed0aa224dc5ca811298a
Anthony Edmonds <anthony.edmonds@networkrail.co.uk>
2021-11-29 08:38:06 +0000

    Amended impersonation

--- src/resources/views/parts/impersonation.blade.php
+++ src/resources/views/parts/impersonation.blade.php
@@ -1,6 +1,6 @@
 @impersonating
     <x-govuk::phase-banner phase="Impersonating">
         You are currently impersonating {{ \Illuminate\Support\Facades\Auth::user()->name }}.
-        <x-govuk::a href="{{ route('admin.impersonate.leave') }}">Press here to leave impersonation</x-govuk::a>.
+        <x-govuk::a href="{{ route('impersonate.leave') }}">Press here to leave impersonation</x-govuk::a>.
     </x-govuk::phase-banner>
 @endImpersonating
