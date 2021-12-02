32dc04386535f345aa20faac6df4a66c1398606e
Anthony Edmonds <anthony.edmonds@networkrail.co.uk>
2021-12-02 09:17:12 +0000

    Added form helper for retrieving models from the session

---
+++ src/app/Helpers/GovukForm.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AnthonyEdmonds\GovukLaravel\Helpers;
+
+use ErrorException;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Session;
+
+/* Models can be created over several form steps using the User's session */
+class GovukForm
+{
+    /* Put a form model into the User's session */
+    public static function set(string $key, Model $model): void
+    {
+        Session::put($key, $model);
+    }
+    
+    /* Retrieve a form model from the User's session */
+    public static function get(string $key): Model
+    {
+        if (Session::has($key) === false) {
+            throw new ErrorException('The form you are trying to access has expired. Please start again.');
+        }
+
+        return Session::get($key);
+    }
+}
