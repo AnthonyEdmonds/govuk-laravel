f70b127ce64e53d418f02868ef0896e6224c750f
Anthony Edmonds <anthony.edmonds@networkrail.co.uk>
2021-12-06 08:52:43 +0000

    Added Words rule for word-based validation

---
+++ src/app/Rules/Words.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AnthonyEdmonds\GovukLaravel\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class Words implements Rule
+{
+    public function __construct(protected int $words)
+    {}
+
+    public function passes($attribute, $value): bool
+    {
+        return substr_count($value, ' ') <= $this->words;
+    }
+    
+    public function message(): string
+    {
+        return "The :attribute must be {$this->words} words or fewer";
+    }
+}
