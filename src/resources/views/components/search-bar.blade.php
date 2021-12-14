130b872cf6bca5a6ab2b20d57397f0eeccd61b86
Anthony Edmonds <anthony.edmonds@networkrail.co.uk>
2021-12-14 10:43:32 +0000

    Updated search bar scss and props

--- src/resources/views/components/search-bar.blade.php
+++ src/resources/views/components/search-bar.blade.php
@@ -1,8 +1,8 @@
 @props([
     'id' => $name,
     'label' => 'Search',
-    'name' => 'search_term',
-    'searchTerm' => null,
+    'name',
+    'value' => null,
 ])
 
 <x-govuk::form-group name="{{ $name }}">
@@ -17,15 +17,13 @@
             name="{{ $name }}"
             spellcheck="false"
             type="search"
-            value="{{ old($name, $searchTerm) }}"
+            value="{{ old($name, $value) }}"
         />
 
         <x-govuk::button>
             <svg
                 xmlns='http://www.w3.org/2000/svg'
                 viewBox='0 0 36 36'
-                width='40'
-                height='40'
             >
                 <path
                     d='M25.7 24.8L21.9 21c.7-1 1.1-2.2 1.1-3.5 0-3.6-2.9-6.5-6.5-6.5S10 13.9 10 17.5s2.9 6.5 6.5 6.5c1.6 0 3-.6 4.1-1.5l3.7 3.7 1.4-1.4zM12 17.5c0-2.5 2-4.5 4.5-4.5s4.5 2 4.5 4.5-2 4.5-4.5 4.5-4.5-2-4.5-4.5z'
