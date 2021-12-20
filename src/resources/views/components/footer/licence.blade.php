d944545aa970f766e8cbec1f301ed7f30c7feb06
Anthony Edmonds <anthony.edmonds@networkrail.co.uk>
2021-12-20 11:24:31 +0000

    Made licence logo optional

--- src/resources/views/components/footer/licence.blade.php
+++ src/resources/views/components/footer/licence.blade.php
@@ -4,13 +4,15 @@
     'width' => null
 ])
 
-<img
-    aria-hidden="true"
-    class="govuk-footer__licence-logo"
-    height="{{ $height }}"
-    src="{{ $logo }}"
-    width="{{ $width }}"
-/>
+@if($logo !== null)
+    <img
+        aria-hidden="true"
+        class="govuk-footer__licence-logo"
+        height="{{ $height }}"
+        src="{{ $logo }}"
+        width="{{ $width }}"
+    />
+@endif
 
 <span class="govuk-footer__licence-description">
     {{ $slot }}
