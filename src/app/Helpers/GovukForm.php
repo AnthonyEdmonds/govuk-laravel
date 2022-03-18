1944aa0db0c7b970c41049faf6d34ebe6c37e0f7
Anthony Edmonds <anthony.edmonds@live.co.uk>
2022-03-18 08:56:24 +0000

    Expanded GovUKForm to cover arrays

--- src/app/Helpers/GovukForm.php
+++ src/app/Helpers/GovukForm.php
@@ -10,13 +10,13 @@ use Illuminate\Support\Facades\Session;
 class GovukForm
 {
     /* Put a form model into the User's session */
-    public static function set(string $key, Model $model): void
+    public static function set(string $key, Model|array $form): void
     {
-        Session::put($key, $model);
+        Session::put($key, $form);
     }
 
     /* Retrieve a form model from the User's session */
-    public static function get(string $key): Model
+    public static function get(string $key): Model|array
     {
         if (Session::has($key) === false) {
             throw new ErrorException('The form you are trying to access has expired. Please start again.');
