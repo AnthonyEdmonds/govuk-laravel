4c241c4c66cdc626c37110df777e1262f7c37e51
Stephen Oke <stephen.oke@networkrail.co.uk>
2022-10-18 10:24:39 +0100

    Added test for Id method

---
+++ tests/Unit/Questions/Question/IsInLineTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace AnthonyEdmonds\GovukLaravel\Tests\Unit\Questions\Question;
+
+use AnthonyEdmonds\GovukLaravel\Questions\Question;
+use AnthonyEdmonds\GovukLaravel\Tests\TestCase;
+
+class IsInLineTest extends TestCase
+{
+    protected Question $question;
+
+
+
+}
