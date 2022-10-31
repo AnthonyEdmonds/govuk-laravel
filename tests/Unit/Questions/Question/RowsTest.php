693deb9efa147e88b72724eb252989817d7a3954
Stephen Oke <stephen.oke@networkrail.co.uk>
2022-10-31 12:15:53 +0000

    Added test for Rows method

---
+++ tests/Unit/Questions/Question/RowsTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace AnthonyEdmonds\GovukLaravel\Tests\Unit\Questions\Question;
+
+use AnthonyEdmonds\GovukLaravel\Questions\Question;
+use AnthonyEdmonds\GovukLaravel\Tests\TestCase;
+
+class RowsTest extends TestCase
+{
+    protected Question $question;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->question = new Question('Duck', 'Dave', Question::CHECKBOXES);
+        $this->question->rows(7);
+    }
+
+    public function testRows(): void
+    {
+        $this->assertEquals(7, $this->question->rows);
+    }
+}
