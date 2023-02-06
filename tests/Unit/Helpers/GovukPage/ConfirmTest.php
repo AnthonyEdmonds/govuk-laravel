673b5cabdd0693f9ea58787e3cc363bea9d89b67
Sim Roberts <simran.roberts@networkrail.co.uk>
2023-02-06 13:29:53 +0000

    This is a test of the static function confirm on the GovukPage

---
+++ tests/Unit/Helpers/GovukPage/ConfirmTest.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace AnthonyEdmonds\GovukLaravel\Tests\Unit\Helpers\GovukPage;
+
+use AnthonyEdmonds\GovukLaravel\Helpers\GovukPage;
+use AnthonyEdmonds\GovukLaravel\Pages\Page;
+use AnthonyEdmonds\GovukLaravel\Tests\TestCase;
+
+class ConfirmTest extends TestCase
+{
+    protected array $page;
+
+    protected function makePage(
+        string|null $otherButtonLabel = 'carrot',
+        string|null $otherButtonHref = 'kitten',
+    ): void {
+        $this->page = GovukPage::confirm(
+            'good',
+            'for',
+            'apples',
+            'table',
+            'let',
+            'potato',
+            $otherButtonLabel,
+            $otherButtonHref,
+            'warning',
+            'beagle',
+        )->toArray();
+    }
+
+    public function testHasTitle(): void
+    {
+        $this->makePage();
+
+        $this->assertEquals(
+            'good',
+            $this->page['title'],
+        );
+    }
+
+    public function testHasBlade(): void
+    {
+        $this->makePage();
+
+        $this->assertEquals(
+            'for',
+            $this->page['content'],
+        );
+    }
+
+    public function testHasSubmitButtonLabel(): void
+    {
+        $this->makePage();
+
+        $this->assertEquals(
+            'apples',
+            $this->page['submitButtonLabel'],
+        );
+    }
+
+    public function testHasAction(): void
+    {
+        $this->makePage();
+
+        $this->assertEquals(
+            'table',
+            $this->page['action'],
+        );
+    }
+
+    public function testHasBack(): void
+    {
+        $this->makePage();
+
+        $this->assertEquals(
+            'let',
+            $this->page['back'],
+        );
+    }
+
+    public function testHasMethod(): void
+    {
+        $this->makePage();
+
+        $this->assertEquals(
+            "potato",
+            $this->page['method'],
+        );
+    }
+
+    public function testHasOtherButtonLabel(): void
+    {
+        $this->makePage();
+
+        $this->assertEquals(
+            'carrot',
+            $this->page['otherButtonLabel'],
+        );
+    }
+
+    public function testHasOtherButtonHref(): void
+    {
+        $this->makePage();
+
+        $this->assertEquals(
+            'kitten',
+            $this->page['otherButtonHref'],
+        );
+    }
+
+    public function testHasSubmitButtonType(): void
+    {
+        $this->makePage();
+
+        $this->assertEquals(
+            'warning',
+            $this->page['submitButtonType'],
+        );
+    }
+
+    public function testHasCaption(): void
+    {
+        $this->makePage();
+
+        $this->assertEquals(
+            'beagle',
+            $this->page['caption'],
+        );
+    }
+
+    public function testOtherButtonHrefIsBackWhenNull(): void
+    {
+        $this->makePage(null, null);
+
+        $this->assertEquals(
+            'let',
+            $this->page['otherButtonHref'],
+        );
+    }
+
+    public function testOtherButtonLabelIsTrueWhenNull(): void
+    {
+        $this->makePage(null, null);
+
+        $this->assertEquals(
+            'Cancel and back',
+            $this->page['otherButtonLabel'],
+        );
+    }
+}
