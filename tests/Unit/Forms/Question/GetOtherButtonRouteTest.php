50a32ac8e93c4664f3c01c29d13bea739f0db1cb
Anthony Edmonds <anthony.edmonds@live.co.uk>
2023-04-26 15:03:32 +0100

    Fixed test before revision tests

--- tests/Unit/Forms/Question/GetOtherButtonRouteTest.php
+++ tests/Unit/Forms/Question/GetOtherButtonRouteTest.php
@@ -2,24 +2,29 @@
 
 namespace AnthonyEdmonds\GovukLaravel\Tests\Unit\Forms\Question;
 
+use AnthonyEdmonds\GovukLaravel\Forms\Form;
 use AnthonyEdmonds\GovukLaravel\Tests\Forms\Questions\FirstQuestion;
+use AnthonyEdmonds\GovukLaravel\Tests\Forms\TestForm;
 use AnthonyEdmonds\GovukLaravel\Tests\TestCase;
 
 class GetOtherButtonRouteTest extends TestCase
 {
+    protected TestForm $form;
+
     protected FirstQuestion $question;
 
     protected function setUp(): void
     {
         parent::setUp();
 
+        $this->form = new TestForm();
         $this->question = new FirstQuestion();
     }
 
     public function testReturnsNull(): void
     {
         $this->assertNull(
-            $this->question->getOtherButtonRoute()
+            $this->question->getOtherButtonRoute($this->form, Form::NEW),
         );
     }
 }
