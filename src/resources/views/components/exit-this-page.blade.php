ee4b0c97ff2833235932b1203ae98f5f9a84379d
Anthony Edmonds <anthony.edmonds@networkrail.co.uk>
2023-07-13 13:51:09 +0100

    Added exit this page function

---
+++ src/resources/views/components/exit-this-page.blade.php
@@ -0,0 +1,17 @@
+@props([
+    'href',
+    'label' => 'Exit this page'
+])
+
+<div
+    class="govuk-exit-this-page"
+    data-module="govuk-exit-this-page"
+>
+    <a
+        href="{{ $href }}"
+        role="button"
+        draggable="false"
+        class="govuk-button govuk-button--warning govuk-exit-this-page__button govuk-js-exit-this-page-button"
+        data-module="govuk-button"
+    >{{ $label }}</a>
+</div>
