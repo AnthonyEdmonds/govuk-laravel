47c4abd07134d55d16b5caad0834e57b1f3e3796
Anthony Edmonds <anthony.edmonds@networkrail.co.uk>
2024-02-28 14:06:02 +0000

    Amended button style and method

--- src/resources/views/templates/start.blade.php
+++ src/resources/views/templates/start.blade.php
@@ -15,7 +15,9 @@
                 @if($isInProgress === true)
                     <x-govuk::button
                         form-action="{{ $summaryRoute }}"
+                        form-method="{{ \AnthonyEdmonds\GovukLaravel\Pages\Page::GET_METHOD }}"
                         prevent-double-click
+                        secondary
                     >Resume previous session</x-govuk::button>
                 @endif
             @endisset
