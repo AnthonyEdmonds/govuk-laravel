15cf9a988797ee111444b93005b176f27ee992a9
Simran <srobert5@networkrail.co.uk>
2024-04-30 10:47:43 +0100

    added changes for the summary list and item blade

--- src/resources/views/components/summary-list.blade.php
+++ src/resources/views/components/summary-list.blade.php
@@ -6,14 +6,16 @@
 @php
     $defaultAction = null;
     $listClasses = 'govuk-summary-list';
+    $mixedList = false;
 
     if ($noBorders === true) {
         $listClasses .= ' govuk-summary-list--no-border';
     }
-     
+
     foreach ($list as $item) {
         if (isset($item['action']) === true) {
             $defaultAction = true;
+            $mixedList = true;
             break;
         }
     }
@@ -22,9 +24,10 @@
 <dl class="{{ $listClasses }}">
     @foreach($list as $key => $data)
         <x-govuk::summary-list.item
-            :key="$key"
-            :value="$data['value'] ?? $data"
-            :action="$data['action'] ?? $defaultAction"
+                :key="$key"
+                :value="$data['value'] ?? $data"
+                :action="$data['action'] ?? $defaultAction"
+                :mixed-list="$mixedList"
         />
     @endforeach
 </dl>
