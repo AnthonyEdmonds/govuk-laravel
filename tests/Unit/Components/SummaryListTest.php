c6c8d6b2ddc504a8f2855d925ad8068b1ff1bfd6
Simran <srobert5@networkrail.co.uk>
2024-05-03 08:30:45 +0100

    removed some tests from summary list stub

---
+++ tests/Unit/Components/SummaryListTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AnthonyEdmonds\GovukLaravel\Tests\Unit\Components;
+
+use AnthonyEdmonds\GovukLaravel\Tests\TestCase;
+use NunoMaduro\LaravelMojito\ViewAssertion;
+
+class SummaryListTest extends TestCase
+{
+    public function testHasSummaryList(): void
+    {
+        $this->makeSummaryList()
+            ->first('div.govuk-summary-card__content')
+            ->first('div')
+            ->hasClass('govuk-summary-list');
+    }
+
+    protected function makeSummaryList(): ViewAssertion
+    {
+        $this->setViewAttributes();
+
+        return $this->assertView('govuk::components.summary-list', [
+            'key' => 'Test',
+            'value' => [
+                'Coca' => 'Cola',
+            ],
+            'action' => true,
+            'mixedList' => true,
+        ]);
+    }
+}
