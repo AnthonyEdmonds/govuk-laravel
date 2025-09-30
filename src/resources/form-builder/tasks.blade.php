5ede861f2ede99967aad9e33b811c0e6daa6829e
Anthony Edmonds <anthony.edmonds@live.co.uk>
2025-09-30 09:12:59 +0100

    Added task groups to template

--- src/resources/form-builder/tasks.blade.php
+++ src/resources/form-builder/tasks.blade.php
@@ -1,5 +1,24 @@
 @extends('govuk::layout.page')
 
+@php
+    $hasGroups = false;
+    $groups = [];
+
+    foreach ($tasks as $task) {
+        $group = $task['group'];
+
+        if ($group !== null) {
+            $hasGroups = true;
+        }
+
+        if (array_key_exists($group, $groups) === false) {
+            $groups[$group] = [];
+        }
+
+        $groups[$group][] = $task;
+    }
+@endphp
+
 @section('before-main')
     @foreach ($description as $line)
         <x-govuk::p>{{ $line }}</x-govuk::p>
@@ -7,7 +26,16 @@
 @endsection
 
 @section('main')
-    <x-govuk::task-list :tasks="$tasks" />
+    @if($hasGroups === true)
+        @foreach($groups as $label => $group)
+            <x-govuk::task-list
+                :tasks="$group"
+                :title="$label"
+            />
+        @endforeach
+    @else
+        <x-govuk::task-list :tasks="$tasks" />
+    @endif
 
     @isset($draft)
         <x-govuk::form action="">
@@ -15,19 +43,19 @@
 
             <x-govuk::button-group>
                 <x-govuk::a
-                        as-button
-                        href="{{ $actions['summary']->link }}"
+                    as-button
+                    href="{{ $actions['summary']->link }}"
                 >{{ $actions['summary']->label }}</x-govuk::a>
 
                 @isset($draft)
                     <x-govuk::button
-                            form-action="{{ $draft->link }}"
-                            secondary
+                        form-action="{{ $draft->link }}"
+                        secondary
                     >{{ $draft->label }}</x-govuk::button>
                 @endisset
 
                 <x-govuk::a
-                        href="{{ $actions['exit']->link }}"
+                    href="{{ $actions['exit']->link }}"
                 >{{ $actions['exit']->label }}</x-govuk::button>
             </x-govuk::button-group>
 
