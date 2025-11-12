ce02d110363560459d54045c76e094224d2fb889
Anthony Edmonds <anthony.edmonds@live.co.uk>
2025-11-12 09:01:40 +0000

    Refactor tasks.blade.php for improved clarity

--- src/resources/form-builder/tasks.blade.php
+++ src/resources/form-builder/tasks.blade.php
@@ -39,27 +39,28 @@
 
     @isset($draft)
         <x-govuk::form action="">
-            @endisset
+    @endisset
 
-            <x-govuk::button-group>
-                <x-govuk::a
-                    as-button
-                    href="{{ $actions['summary']->link }}"
-                >{{ $actions['summary']->label }}</x-govuk::a>
-
-                @isset($draft)
-                    <x-govuk::button
-                        form-action="{{ $draft->link }}"
-                        secondary
-                    >{{ $draft->label }}</x-govuk::button>
-                @endisset
-
-                <x-govuk::a
-                    href="{{ $actions['exit']->link }}"
-                >{{ $actions['exit']->label }}</x-govuk::button>
-            </x-govuk::button-group>
+        <x-govuk::button-group>
+            <x-govuk::a
+                as-button
+                href="{{ $actions['summary']->link }}"
+            >{{ $actions['summary']->label }}</x-govuk::a>
 
             @isset($draft)
+                <x-govuk::button
+                    form-action="{{ $draft->link }}"
+                    secondary
+                >{{ $draft->label }}</x-govuk::button>
+            @endisset
+
+            <x-govuk::a
+                href="{{ $actions['exit']->link }}"
+            >{{ $actions['exit']->label }}</x-govuk::button>
+        </x-govuk::button-group>
+
+    @isset($draft)
         </x-govuk::form>
     @endisset
 @endsection
+
