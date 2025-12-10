a8d1253f382ba73db6e3be67aed8c14aff944a50
Anthony Edmonds <anthony.edmonds@live.co.uk>
2025-12-10 08:18:37 +0000

    Updated form builder view template

--- docs/forms.md
+++ docs/forms.md
@@ -7,9 +7,13 @@ GOV.UK style blades have been included which override the templates provided by 
 To use them, simply publish the blades using:
 
 ```bash
-php artisan vendor:publish --tag=govuk-form-builder
+php artisan vendor:publish --tag=govuk-form-builder --force
 ```
 
 These will be output to `resources/vendor/form-builder/...` where you may customise them further, if needed.
+
+The `--force` flag is included to ensure your templates are always up to date.
+
+If you have modified the templates, you may omit this flag.
 
 For more information about using LaravelFormBuilder, refer to their documentation.
--- src/resources/form-builder/view.blade.php
+++ src/resources/form-builder/view.blade.php
@@ -1,6 +1,30 @@
 @extends('govuk::layout.page')
+@use(Illuminate\Support\Facades\View)
+
+@php
+    $hasActions = View::hasSection('actions') === true;
+    $hasEdit = isset($edit) === true;
+@endphp
+
+@section('before-main')
+    @if($hasActions === true || $hasEdit === true)
+        <x-govuk::h2>{{ $actionsLabel ?? 'Actions' }}</x-govuk::h2>
+
+        @if($hasActions === true)
+            @yield('actions')
+        @elseif($hasEdit === true)
+            <x-govuk::ul spaced>
+                <li>
+                    <x-govuk::a href="{{ $edit->link }}">{{ $edit->label }}</x-govuk::a>
+                </li>
+            </x-govuk::ul>
+        @endif
+    @endif
+@endsection
 
 @section('main')
+    <x-govuk::h2>{{ $detailsLabel ?? 'Details' }}</x-govuk::h2>
+
     @forelse($summary as $task)
         <x-govuk::summary-card
             :list="$task['list']"
@@ -9,8 +33,4 @@
     @empty
         <p>No tasks have been added to this form.</p>
     @endforelse
-
-    <x-govuk::p>
-        <x-govuk::a href="{{ $edit->link }}">{{ $edit->label }}</x-govuk::a>
-    </x-govuk::p>
 @endsection
