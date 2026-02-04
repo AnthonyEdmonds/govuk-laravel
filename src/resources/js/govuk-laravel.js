ee59a031ec7c5010b620c1b58ed861026c7d1eea
Anthony Edmonds <anthony.edmonds@live.co.uk>
2026-02-04 14:55:46 +0000

    Added button unlock after page change

--- src/resources/js/govuk-laravel.js
+++ src/resources/js/govuk-laravel.js
@@ -24,8 +24,18 @@ document.addEventListener('DOMContentLoaded', function () {
         let buttons = document.getElementsByTagName('button');
 
         for (const button of buttons) {
-            button.setAttribute('aria-disabled', 'false');
-            button.disabled = false;
+            if (button.hasAttribute('data-prevent-double-click') === false) {
+                continue;
+            }
+
+            if (button.getAttribute('data-prevent-double-click') === false) {
+                continue;
+            }
+
+            if (button.disabled === true) {
+                button.setAttribute('aria-disabled', 'false');
+                button.disabled = false;
+            }
         }
     });
 });
