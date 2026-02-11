ecb48c13e5e4dc90342ea77755ba1ae066a2a047
Tahreem Asghar <Tahreem.Asghar@networkrail.co.uk>
2026-02-11 09:58:21 +0000

    Added margin override to h3

--- src/resources/views/components/h3.blade.php
+++ src/resources/views/components/h3.blade.php
@@ -1,9 +1,18 @@
 @props([
     'id' => null,
+    'marginBottom' => null,
     'size' => 's'
 ])
 
+@php
+    $classes = "govuk-heading-$size";
+
+    if ($marginBottom !== null) {
+        $classes .= " govuk-!-margin-bottom-$marginBottom";
+    }
+@endphp
+
 <h3
-    class="govuk-heading-{{ $size }}"
+    class="{{ $classes }}"
     @if($id !== null) id="{{ $id }}" @endif
 >{{ $slot }}</h3>
