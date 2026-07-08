b5ff53d6556944c646664be32d60182d1f13eb02
Anthony Edmonds <anthony.edmonds@live.co.uk>
2026-07-08 09:57:00 +0100

    Added favicons customisation

--- src/resources/views/layout/head.blade.php
+++ src/resources/views/layout/head.blade.php
@@ -10,7 +10,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="theme-color" content="#0b0c0c" />
 
-    @include(config('govuk.favicons'))
+    @include(config('govuk.parts.favicons'))
     
     @vite('resources/js/app.js')
 </head>
